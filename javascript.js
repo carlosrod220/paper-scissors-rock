dc7549258ae60ea68615833d8d9f91a3dfa410df
Carlos Rodriguez <git@github.com:carlosrod220/odin-recipes.git>
2026-05-29 15:47:31 -0700

    completed playround function

--- javascript.js
+++ javascript.js
@@ -1,23 +1,90 @@
+let humanScore = 0;
+let computerScore = 0;
+let numberOfRounds = 5;
+
+
+
+function playRound(humanChoice,computerChoice) {
+
+    if (humanChoice === 'paper' && computerChoice === 'paper') {
+       console.log("It's a paper tie!");
+    }
+    else if (humanChoice === 'paper' &&  computerChoice === 'rock') {
+        console.log('You win! Paper beats rock!');
+        humanScore++;
+    }
+    else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+       console.log('You lose! scissors cuts paper!');
+       computerScore++;
+    }
+    else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log('You win! rock beats paper!');
+        humanScore++;
+    }
+    else if (humanChoice === 'rock' && computerChoice === 'rock') {
+        console.log("It's a rock tie!");    
+    }
+    else if (humanChoice === 'rock' && computerChoice === 'paper'){
+        console.log('You lose! paper beats rock!');
+        computerScore++;
+    }
+    else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        console.log('You lose! rock beats scissors');
+        computerScore++
+    }
+    else if (humanChoice ==='scissors' && computerChoice === 'scissors') {
+        console.log("It's a scissors tie!")
+    }
+    else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log('You win! scissors cut paper!')
+        humanScore++;
+    }
+
+}   
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+
+
+console.log('Computer Score: ' + ' ' + computerScore);
+console.log('Your Score: ' + ' ' + humanScore); 
+
+
+
+
+
+
+
+
+
+
+
+
+
 function randomInt(max) {
     return Math.floor(Math.random(3) * max);
 }
 
-
-function getComputerChoice() {
+function getComputerChoice() { 
+   let computerChoice;
    if (randomInt(3) === 0) {
-    console.log('paper');
+    computerChoice = 'paper';
    }
    else if (randomInt(3) === 1){
-    console.log('rock');
+    computerChoice = 'rock';
    }
-   else {
-    console.log('scissors');
+   else if (randomInt(3) === 2) {
+    computerChoice = 'scissors';
    }
+   return computerChoice;
 }
 
 function getHumanChoice() {
-   let choice =  prompt('rock, paper or scissors: ');
-   return choice;
+   let humanChoice =  prompt('rock, paper or scissors: ');
+    return humanChoice; 
 }
 
-console.log(getHumanChoice());
+
+
