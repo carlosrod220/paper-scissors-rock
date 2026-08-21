947b8f6d9b237473be35fc42bac073b95ab4d695
Carlos Rodriguez <git@github.com:carlosrod220/odin-recipes.git>
2026-08-21 16:05:33 -0700

    updated the JS to streamline it

--- javascript.js
+++ javascript.js
@@ -12,7 +12,7 @@ const btnscissors = document.getElementById('scissors');
 btnscissors.addEventListener('click', getHumanChoice);
 
 const humantracker = document.createElement('div');
-humantracker.id = "scoreres"
+humantracker.id = "scoreres";
 humantracker.textContent = "Your Score: " + humanScore;
 document.body.appendChild(humantracker);
 
@@ -20,56 +20,53 @@ const computertracker = document.createElement('div');
 computertracker.textContent = 'Computer Score: ' + computerScore;
 document.body.appendChild(computertracker);
 
+const winnerAnnouncement = document.getElementById("notice");
 
 function getHumanChoice(event) {
 
     let humanChoice = event.target.textContent; 
     let computerChoice = getComputerChoice();
     playRound(humanChoice, computerChoice);
-    
-    // console.log('Computer Score: ' + ' ' + computerScore);
-    // console.log('Your Score: ' + ' ' + humanScore); 
 }
-
 
 function playRound(humanChoice, computerChoice) {
 
     if (humanChoice === 'paper' && computerChoice === 'paper') {
-       console.log("It's a paper tie!");
+       winnerAnnouncement.textContent = "It's a paper tie!";
     }
     else if (humanChoice === 'paper' &&  computerChoice === 'rock') {
-        console.log('You win! Paper beats rock!');
+        winnerAnnouncement.textContent = 'You win! Paper beats rock!';
         humanScore++;
         humantracker.textContent = "Your Score: " + humanScore;
     }
     else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-       console.log('You lose! scissors cuts paper!');
+       winnerAnnouncement.textContent = 'You lose! scissors cuts paper!';
        computerScore++;
        computertracker.textContent = 'Computer Score: ' + computerScore;
     }
     else if (humanChoice === 'rock' && computerChoice === 'scissors') {
-        console.log('You win! rock beats scissors!');
+        winnerAnnouncement.textContent = 'You win! rock beats scissors!';
         humanScore++;
         humantracker.textContent = "Your Score: " + humanScore;
     }
     else if (humanChoice === 'rock' && computerChoice === 'rock') {
-        console.log("It's a rock tie!");    
+        winnerAnnouncement.textContent = "It's a rock tie!";    
     }
     else if (humanChoice === 'rock' && computerChoice === 'paper'){
-        console.log('You lose! paper beats rock!');
+        winnerAnnouncement.textContent = 'You lose! paper beats rock!';
         computerScore++;
         computertracker.textContent = 'Computer Score: ' + computerScore;
     }
     else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-        console.log('You lose! rock beats scissors');
-        computerScore++
+        winnerAnnouncement.textContent = 'You lose! rock beats scissors';
+        computerScore++;
         computertracker.textContent = 'Computer Score: ' + computerScore;
     }
     else if (humanChoice ==='scissors' && computerChoice === 'scissors') {
-        console.log("It's a scissors tie!")
+        winnerAnnouncement.textContent = "It's a scissors tie!";
     }
     else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-        console.log('You win! scissors cut paper!')
+        winnerAnnouncement.textContent = 'You win! scissors cut paper!';
         humanScore++;
          humantracker.textContent = "Your Score: " + humanScore;
     }
